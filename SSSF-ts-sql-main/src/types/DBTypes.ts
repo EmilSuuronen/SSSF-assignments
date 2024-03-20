18a4b33ad5720057aeef9bdbb4c1d5f64f0068bd
EmilSuuronen <emilsuuronen@gmail.com>
2024-03-20 10:26:16 +0200

    created the rest API

---
+++ SSSF-ts-sql-main/src/types/DBTypes.ts
@@ -0,0 +1,20 @@
+type Category = {
+  category_id: number;
+  category_name: string;
+};
+
+type Species = {
+  species_id: number;
+  species_name: string;
+  category: number;
+  image: string;
+};
+
+type Animals = {
+  animal_id: number;
+  animal_name: string;
+  species: number;
+  birtdate: string;
+};
+
+export {Category, Species, Animals};
